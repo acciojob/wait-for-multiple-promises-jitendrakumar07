962524043b5af1f06495753d36637818fd28d026
JITENDRA KUMAR <jitendrakumar77800@gmail.com>
2025-03-06 11:33:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+function createPromise(promiseNumber) {
+    const delay = Math.random() * 2000 + 1000; 
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({ promiseNumber, time: (delay / 1000).toFixed(3) }); 
+        }, delay);
+    });
+}
+
+const promises = [
+    createPromise(1),
+    createPromise(2),
+    createPromise(3)
+];
+
+const outputTable = document.getElementById("output");
+
+
+outputTable.innerHTML = '<tr><td colspan="2">Loading...</td></tr>';
+
+Promise.all(promises).then(results => {
+    
+    outputTable.innerHTML = '';
+
+    let totalTime = 0;
+    results.forEach(result => {
+        totalTime = Math.max(totalTime, result.time); 
+        const row = document.createElement('tr');
+        row.innerHTML = `<td>Promise ${result.promiseNumber}</td><td>${result.time}</td>`;
+        outputTable.appendChild(row);
+    });
+
+  
+    const totalRow = document.createElement('tr');
+    totalRow.innerHTML = `<td>Total</td><td>${totalTime}</td>`;
+    outputTable.appendChild(totalRow);
+});
+
